641ef332fb6805a62c452f8cdb1173bb375418c5
Almat Begaidarov <aleke7.kz@gmail.com>
2022-02-24 00:40:46 +0600

    lab1

---
+++ lab1/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        
+        setContentView(R.layout.activity_main)
+    }
+
+}
